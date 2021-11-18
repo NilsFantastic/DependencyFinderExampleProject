e804e3714342d29af1aad3691619559386b692a2
Nils Stjernstrid <nils.stjernstrid@almostfantastic.com>
2021-11-18 11:15:46 +0100

    Adding some documentation for ease of use

--- Assets/Plugins/DependencyFinder/Attribute/FromPrefabAttribute.cs
+++ Assets/Plugins/DependencyFinder/Attribute/FromPrefabAttribute.cs
@@ -1,4 +1,6 @@
 ﻿using System;
-
+/// <summary>
+/// Finds a single prefab in the assets directory with a component matching the type of the field carrying this attribute on it's topmost object (the prefab itself)
+/// </summary>
 [AttributeUsage(AttributeTargets.Field)]
 public class FromPrefabAttribute : UnityEngine.PropertyAttribute { }
--- Assets/Plugins/DependencyFinder/Attribute/FromSceneAttribute.cs
+++ Assets/Plugins/DependencyFinder/Attribute/FromSceneAttribute.cs
@@ -1,4 +1,8 @@
 ﻿using System;
 
+/// <summary>
+/// Finds a single component in the scene with the script matching the type of the field carrying this attribute.
+/// It will first look on the component itself, if not found there it will go on and look in the entire hierarchy
+/// </summary>
 [AttributeUsage(AttributeTargets.Field)]
 public class FromSceneAttribute : UnityEngine.PropertyAttribute { }
